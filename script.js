359dbe1c6a2373ea2e52937fb4fb6afd5dbb49e9
metaluxx2026 <metaluxx2026@gmail.com>
2026-03-07 18:08:24 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Content of script.js
-// Example corrected content with 'src' instead of 'srt'
-
-console.log('Loading resources from src');
-console.log('Script loaded from src');
-// Further implementation here
